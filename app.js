d20c178c850a01e189bd3db7d697b2bc235552f2
Michal Kolanecki <michalkolanecki@gmail.com>
2021-08-19 16:09:44 +0200

    Create functionality which add task to the list

---
+++ app.js
@@ -0,0 +1,19 @@
+const addForm = document.querySelector('.add');
+const formList = document.querySelector('.todos');
+
+const createTemplate = addTask => {
+    const addHtml = `
+        <li class="list-group-item d-flex justify-content-between align-items-center text-light">
+            <span>${addTask}</span>
+            <i class="far fa-trash-alt delete"></i>
+        </li> 
+    `;
+    
+    formList.innerHTML += addHtml;
+}
+
+addForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const addTask = addForm.add.value.trim();
+    createTemplate(addTask);
+});
