faed38043b4078732e9cf2eab3a7167f9f322fea
Michal Kolanecki <michalkolanecki@gmail.com>
2021-08-19 16:33:56 +0200

    Add functionality which delete task after click in icon

--- app.js
+++ app.js
@@ -21,3 +21,9 @@ addForm.addEventListener('submit', e => {
         addForm.reset();
     }
 });
+
+formList.addEventListener('click', e => {
+    if( e.target.classList.contains('delete') ) {
+        e.target.parentElement.remove();
+    }
+});
